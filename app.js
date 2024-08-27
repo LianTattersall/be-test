21d653bfb066802821b8e15f522afb49a0d3598a
Lian Tattersall <liantattersall@Lians-iMac.lan>
2024-08-27 17:37:27 +0100

    config var

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require("express");
 
 const app = express();
 
+const firebase = process.env.FIREBASE;
+
 app.get("/api", (req, res) => {
   res.status(200).send({ hello: "world" });
+});
+
+app.get("/api/firebase", (req, res) => {
+  res.status(200).send(firebase);
 });
 
 app.listen(9000, () => {
